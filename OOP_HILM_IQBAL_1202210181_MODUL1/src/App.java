c0f29fecfa510c1ca0a473cbe65c45fe2d12ab18
IqbalInsanKurnia <91659708+IqbalInsanKurnia@users.noreply.github.com>
2022-10-31 08:36:13 +0700

    Add files via upload

---
+++ OOP_HILM_IQBAL_1202210181_MODUL1/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Database {
+    public static void name(String[] args) {
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Register Terlebi Dahulu");
+        System.out.println("=====================================");
+           
+        String nama;
+        String noHp;
+
+        Scanner Userinput = new Scanner(System.in);
+        System.out.print("Masukkan Nilai Tubes :");
+        nama = Userinput.getnama();
+    }
+    }
+}
